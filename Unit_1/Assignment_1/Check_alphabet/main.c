0655b0d9f0d18de0861461143dc8fbcb7b8812c0
mohamed reda <eng.mohmedreda22@gmail.com>
2023-09-29 10:10:32 +0300

    Check_alphabet

---
+++ Unit_1/Assignment_1/Check_alphabet/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char c;
+    while(1)
+    {
+        printf("Enter a character: ");
+        fflush(stdin);fflush(stdout);
+        scanf("%c",&c);
+        if(c >= 65 && c<=90 || c>=97 && c<=122)
+        {
+            printf("%c is an alphabet\n",c);
+        }
+        else
+        {
+            printf("%c is not an alphabet\n",c);
+        }
+    }
+
+    return 0;
+}
