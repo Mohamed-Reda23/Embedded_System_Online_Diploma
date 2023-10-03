d224e88363edab7d86b28701882bba2efc22c651
mohamed reda <eng.mohmedreda22@gmail.com>
2023-10-03 17:08:05 +0300

    Ex1

--- Unit_1/Assignment_5/Ex1/main.c
+++ Unit_1/Assignment_5/Ex1/main.c
@@ -1,1 +1,30 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 30, 2023
+ *      Author: ENG.Mohamed
+ */
 
+#include"stdio.h"
+
+int main()
+{
+	int m=29;
+	int*ab;
+	printf("Address of m : %p\n",&m);
+	printf("value of m : %d\n",m);
+	ab=&m;
+	printf("\nNow ab is assigned with the address of m.\n");
+	printf("Address of pointer ab : %p\n",ab);
+	printf("content of pointer ab : %d\n",*ab);
+	m=34;
+	printf("\nThe value of m assigned to 34 now.\n");
+	printf("Address of pointer ab : %p\n",ab);
+	printf("content of pointer ab : %d\n",*ab);
+	*ab=7;
+	printf("\nThe pointer variable ab is assigned with the value of 7 now.\n");
+	printf("Address of m : %p\n",&m);
+	printf("value of m : %d\n",m);
+
+	return 0;
+}
