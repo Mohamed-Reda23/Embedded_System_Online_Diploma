d92f73ce9468c8c643ede9834bbf34ab56f79794
mohamed reda <eng.mohmedreda22@gmail.com>
2023-10-03 15:13:59 +0300

    Ex3

--- Unit_1/Assignment_3/Ex3/main.c
+++ Unit_1/Assignment_3/Ex3/main.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static int i=0;
+
+
+void reverse_string(char*ptr,int n)
+{
+
+    int m;
+    int temp;
+    temp=ptr[i];
+    ptr[i]=ptr[n-i-1];
+    ptr[n-i-1]=temp;
+    m=n/2;
+    i++;
+    if(i<m)
+    {
+
+
+            reverse_string(ptr,n);
+
+    }
+
+
+}
+
+int main()
+{
+    char arr[100];
+    printf("Enter a sentence : ");
+    gets(arr);
+    int l;
+    l=strlen(arr);
+    reverse_string(arr,l);
+    printf("\n%s\n",arr);
+    return 0;
+}
 
