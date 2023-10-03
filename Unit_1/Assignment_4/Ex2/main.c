5cc0369d9cabbf46673d49f8e665ee39580aa7e7
mohamed reda <eng.mohmedreda22@gmail.com>
2023-10-03 15:56:24 +0300

    Ex2

--- Unit_1/Assignment_4/Ex2/main.c
+++ Unit_1/Assignment_4/Ex2/main.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct student
+{
+    char name[20];
+    int roll;
+    float marks;
+}s;
+
+struct student Read_data(void)
+{
+    struct student a;
+    printf("Enter name: ");
+    gets(a.name);
+    printf("Enter roll number: ");
+    scanf("%d",&a.roll);
+    printf("Enter marks: ");
+    scanf("%f",&a.marks);
+
+    return a;
+}
+
+void print_data(struct student a)
+{
+    printf("Name: %s\n",a.name);
+    printf("Roll: %d\n",a.roll);
+    printf("Marks: %.2f\n",a.marks);
+}
+
+int main()
+{
+    printf("Enter information of students:\n\n");
+    s=Read_data();
+    printf("\nDisplaying Information:\n");
+    print_data(s);
+    return 0;
+}
 
