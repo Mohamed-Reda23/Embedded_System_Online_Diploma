c40d412795e1a116644a9cf1d85c2db16e301d77
mohamed reda <eng.mohmedreda22@gmail.com>
2023-10-03 17:08:50 +0300

    Ex2

--- Unit_1/Assignment_5/Ex2/main.c
+++ Unit_1/Assignment_5/Ex2/main.c
@@ -1,1 +1,24 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 30, 2023
+ *      Author: ENG.Mohamed
+ */
+#define limit  25
+
+#include"stdio.h"
+
+
+int main()
+{
+	char ch='A';
+	char*p=&ch;
+	int i=0;
+	while(i<=limit)
+	{
+		printf("%c ",*p+i);
+		i++;
+	}
+	return 0;
+}
 
