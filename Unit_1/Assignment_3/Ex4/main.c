85134f850a7e14571d492cf5110c0417f53b33e2
mohamed reda <eng.mohmedreda22@gmail.com>
2023-10-03 15:14:41 +0300

    Ex4

--- Unit_1/Assignment_3/Ex4/main.c
+++ Unit_1/Assignment_3/Ex4/main.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int calc_power(int base, int pow)
+{
+    if(pow==0)
+    {
+        return 1;
+    }
+    return base*calc_power(base,pow-1);
+}
+
+int main()
+{
+    int base,power,x;
+    printf("Enter base number: ");
+    fflush(stdin); fflush(stdout);
+    scanf("%d",&base);
+    printf("Enter power number(positive integer): ");
+    fflush(stdin); fflush(stdout);
+    scanf("%d",&power);
+    x=calc_power(base,power);
+    printf("%d^%d = %d\n",base,power,x);
+    return 0;
+}
 
