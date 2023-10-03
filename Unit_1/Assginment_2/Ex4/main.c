558b07fd3470b237931cc8135acf3688cbe174b3
mohamed reda <eng.mohmedreda22@gmail.com>
2023-10-03 14:54:36 +0300

    Ex4

--- Unit_1/Assginment_2/Ex4/main.c
+++ Unit_1/Assginment_2/Ex4/main.c
@@ -1,1 +1,42 @@
+/*
+
+ * main.c
+ *
+ *  Created on: Sep 11, 2023
+ *      Author: ENG.Mohamed
+ */
+
+/*
+Searching the element
+*/
+
+#include"stdio.h"
+
+int main()
+{
+	int arr[10],num,element;
+	printf("Enter number of elements : ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d",&num);
+	for(int i=0;i<num;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(int i=0;i<num;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+	printf("\nEnter the element to be searched : ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d",&element);
+	for(int i=0;i<num;i++)
+	{
+		if(arr[i]==element)
+		{
+			printf("Number found at the location = %d\n",i+1);
+		}
+	}
+
+	return 0;
+}
 
