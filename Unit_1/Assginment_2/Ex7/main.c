a0c9a5a5216095407a5d8c8d9fd8a3f102bb9e83
mohamed reda <eng.mohmedreda22@gmail.com>
2023-10-03 14:57:23 +0300

    Ex7

--- Unit_1/Assginment_2/Ex7/main.c
+++ Unit_1/Assginment_2/Ex7/main.c
@@ -1,1 +1,36 @@
+/*
+
+ * main.c
+ *
+ *  Created on: Sep 11, 2023
+ *      Author: ENG.Mohamed
+ */
+
+/*
+Reverse the string
+*/
+
+#include"stdio.h"
+
+int main()
+{
+	char arr[10];
+	int m=0;
+	char temp;
+	printf("Enter a string : ");
+	fflush(stdin); fflush(stdout);
+	scanf("%s",arr);
+	for(int i=0;arr[i]!='\0';i++)
+	{
+		m++;
+	}
+	for(int i=0;i<m/2;i++)
+	{
+		temp=arr[i];
+		arr[i]=arr[m-i-1];
+		arr[m-i-1]=temp;
+	}
+	printf("Reverse string is : %s\n",arr);
+	return 0;
+}
 
