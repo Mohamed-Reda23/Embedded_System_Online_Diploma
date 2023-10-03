fe4d8760c86ef3c1ac60595a3f44076e3c800d9f
mohamed reda <eng.mohmedreda22@gmail.com>
2023-10-03 17:09:29 +0300

    Ex3

--- Unit_1/Assignment_5/Ex3/main.c
+++ Unit_1/Assignment_5/Ex3/main.c
@@ -1,1 +1,37 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 30, 2023
+ *      Author: ENG.Mohamed
+ */
+
+#include"stdio.h"
+
+
+int main()
+{
+	int s;
+	int*p;
+	printf("Enter the number of elements(max 15): ");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&s);
+	int arr[s];
+	printf("\nInput %d number of elements in the array:\n",s);
+	for(int i=0;i<s;i++)
+	{
+		printf("element-%d:",i+1);
+		fflush(stdin);fflush(stdout);
+		scanf("%d",&arr[i]);
+	}
+	p=arr+s-1;
+	printf("\nThe elements of array in reverse order are :\n");
+	for(int i=s;i>0;i--)
+	{
+		printf("element-%d:%d\n",i,*p);
+		p--;
+	}
+
+	return 0;
+}
+
 
