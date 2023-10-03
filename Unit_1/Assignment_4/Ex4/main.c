e28954c6a078659f22af7b6d1cecf6969f09e7b8
mohamed reda <eng.mohmedreda22@gmail.com>
2023-10-03 15:57:05 +0300

    Ex4

--- Unit_1/Assignment_4/Ex4/main.c
+++ Unit_1/Assignment_4/Ex4/main.c
@@ -1,1 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct students
+{
+    char name[20];
+    int marks;
+}a[10];
+
+int len=sizeof(a)/sizeof(a[0]);
+
+void Read_data(struct students arr[])
+{
+
+    printf("Enter information of students:\n\n");
+    for(int i=0;i<len;i++)
+    {
+        printf("For roll number %d\n",i+1);
+        printf("Enter name: ");
+        fflush(stdin); fflush(stdout);
+        gets(arr[i].name);
+        printf("Enter marks: ");
+        fflush(stdin); fflush(stdin);
+        scanf("%d",&arr[i].marks);
+        printf("\n");
+
+    }
+
+}
+
+void print_data(struct students arr[])
+{
+    printf("Displaying information for of students:\n\n");
+    for(int i=0;i<len;i++)
+    {
+        printf("Information for roll number %d\n",i+1);
+        printf("Name: %s\n",arr[i].name);
+        printf("Marks: %d\n",arr[i].marks);
+        printf("\n");
+    }
+}
+
+
+int main()
+{
+    Read_data(a);
+    printf("\n");
+    print_data(a);
+    return 0;
+}
 
