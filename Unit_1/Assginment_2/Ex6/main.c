9483816b1f996df3f44833e09ff5bf860ca73cd1
mohamed reda <eng.mohmedreda22@gmail.com>
2023-10-03 14:56:39 +0300

    Ex6

--- Unit_1/Assginment_2/Ex6/main.c
+++ Unit_1/Assginment_2/Ex6/main.c
@@ -1,1 +1,31 @@
+/*
+
+ * main.c
+ *
+ *  Created on: Sep 11, 2023
+ *      Author: ENG.Mohamed
+ */
+
+ /*
+ Find the length of string
+ */
+
+#include"stdio.h"
+
+int main()
+{
+	char arr[10];
+	int m=0;
+	int i=0;
+	printf("Enter a string: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%s",arr);
+	printf("\n");
+	for(;arr[i]!='\0';i++)
+	{
+		m++;
+	}
+	printf("Length of string: %d\n",m);
+	return 0;
+}
 
