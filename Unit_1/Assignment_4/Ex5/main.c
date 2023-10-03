cd1b5b2a53e4c9173590ee9d0d2816978f667fc2
mohamed reda <eng.mohmedreda22@gmail.com>
2023-10-03 15:57:33 +0300

    Ex5

--- Unit_1/Assignment_4/Ex5/main.c
+++ Unit_1/Assignment_4/Ex5/main.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define pi    3.14
+#define Area(x)   (x*x*pi)
+
+int main()
+{
+    int radius;
+    float area;
+    printf("Enter the radius: ");
+    fflush(stdin);fflush(stdout);
+    scanf("%d",&radius);
+    area=Area(radius);
+    printf("Area of circule is: %.2f\n",area);
+    return 0;
+}
 
