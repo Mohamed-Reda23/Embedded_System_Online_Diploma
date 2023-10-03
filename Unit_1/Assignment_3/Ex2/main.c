3f25cfc0831e76fe2c819149ad41cd8a7070c71c
mohamed reda <eng.mohmedreda22@gmail.com>
2023-10-03 15:13:21 +0300

    Ex2

--- Unit_1/Assignment_3/Ex2/main.c
+++ Unit_1/Assignment_3/Ex2/main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int Factorial(int num)
+{
+    if(num == 1 || num == 0)
+    {
+        return 1;
+    }
+    return num*Factorial(num-1);
+}
+
+int main()
+{
+    int n,x;
+    printf("Enter the num : ");
+    fflush(stdin); fflush(stdout);
+    scanf("%d",&n);
+    x=Factorial(n);
+    printf("\n");
+    printf("The factorial of %d is : %d\n",n,x);
+    return 0;
+}
 
