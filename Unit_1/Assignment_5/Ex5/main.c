d13f3a1c0c70b220d2708dbafca1044264cacf06
mohamed reda <eng.mohmedreda22@gmail.com>
2023-10-03 17:10:26 +0300

    Ex5

--- Unit_1/Assignment_5/Ex5/main.c
+++ Unit_1/Assignment_5/Ex5/main.c
@@ -1,1 +1,28 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 30, 2023
+ *      Author: ENG.Mohamed
+ */
+
+#include"stdio.h"
+
+struct employee
+{
+	char name[20];
+	int id;
+};
+
+int main()
+{
+	struct employee emp1={"Mohamed",1001};
+	struct employee emp2={"Alex",1002};
+	struct employee emp3={"Wiza",1003};
+
+	struct employee *arr[]={&emp1,&emp2,&emp3};
+	struct employee *(*ptr)[3]=&arr;
+	printf("Employee Name : %s\n",((*(*ptr)))->name);
+	printf("Employee Id : %d\n",(*(*(*ptr))).id);
+	return 0;
+}
 
