508a04c72b24cfba43ad0188e248ac5df6dd4988
mohamed reda <eng.mohmedreda22@gmail.com>
2023-10-03 14:55:20 +0300

    Ex5

--- Unit_1/Assginment_2/Ex5/main.c
+++ Unit_1/Assginment_2/Ex5/main.c
@@ -1,1 +1,36 @@
+/*
 
+ * main.c
+ *
+ *  Created on: Sep 11, 2023
+ *      Author: ENG.Mohamed
+ */
+
+/*
+Find the frequency of character
+*/
+
+#include"stdio.h"
+#include"string.h"
+
+int main()
+{
+	char arr[30],ch;
+	int m=0;
+	printf("Enter a string: ");
+	fflush(stdin); fflush(stdout);
+	gets(arr);
+	printf("\n");
+	printf("Enter a character to find frequency: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%c",&ch);
+	for(int i=0;i<strlen(arr);i++)
+	{
+		if(arr[i]==ch)
+		{
+			m++;
+		}
+	}
+	printf("Frequency of %c = %d\n",ch,m);
+	return 0;
+}
