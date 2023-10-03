6fa8158ee7ecd3df3d4debda8b374a9a90e5ed9f
mohamed reda <eng.mohmedreda22@gmail.com>
2023-10-03 14:50:50 +0300

    Ex1

--- Unit_1/Assginment_2/Ex1/main.c
+++ Unit_1/Assginment_2/Ex1/main.c
@@ -1,1 +1,35 @@
 
+/*
+
+ * main.c
+ *
+ *  Created on: Sep 10, 2023
+ *      Author: ENG.Mohamed
+ */
+
+ /*
+ calculating the average of elements of array
+ */
+
+#include"stdio.h"
+
+int main()
+{
+	int size;
+	float avg;
+	float sum=0.0;
+	printf("Enter the numbers of data:");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&size);
+	printf("\n");
+	float arr[size];
+	for(int i=0;i<size;i++)
+	{
+		printf("%d. Enter data number %d:",i+1,i+1);
+		scanf("%f",&arr[i]);
+		sum+=arr[i];
+	}
+	avg=sum/size;
+	printf("Average = %.2f\n",avg);
+	return 0;
+}
