b8b72a73f198603bfdad3a8049a5a736a076a8be
mohamed reda <eng.mohmedreda22@gmail.com>
2023-10-16 16:33:09 +0300

    uart.c

--- Unit_2/Lesson_2/Lab_2/uart.c
+++ Unit_2/Lesson_2/Lab_2/uart.c
@@ -1,1 +1,16 @@
+#include"uart.h"
+
+/* UART0DR REGISTER */
+#define UART0      ((unsigned int*)0x101F1000)
+#define UART0DR   *((volatile unsigned int*const)(UART0+0x0))
+
+
+void Uart_Send_String(unsigned char*Px)
+{
+    while(*Px!='\0')
+    {
+        UART0DR=(unsigned int)*Px;
+        Px++;
+    }
+}
 
